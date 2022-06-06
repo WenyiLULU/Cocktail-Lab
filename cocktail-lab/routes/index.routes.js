90eeb65eb9e6f0bd0b97202f6a92629b67b1fe84
WenyiLULU <luwenyi1991@gmail.com>
2022-06-06 23:46:32 +0200

    basic style for home page and add details page

--- cocktail-lab/routes/index.routes.js
+++ cocktail-lab/routes/index.routes.js
@@ -6,8 +6,13 @@ const Cocktail = require("../models/Cocktail.model.js");
 router.get("/", async (req, res, next) => {
   const cocktailAlcToShow = await Cocktail.find({category:"Alcoholic"}, null, {limit: 6})
   const cocktailNAlcToShow = await Cocktail.find({category:"Non alcoholic"}, null, {limit: 6})
-  //console.log(cocktailToShow)
+  //console.log(cocktailAlcToShow[0].id)
   res.render("index", {cocktailAlcToShow,cocktailNAlcToShow});
 });
+router.get("/details/:id", async (req, res) => {
+  const cocktailOnClick = await Cocktail.findById(req.params.id)
+  const {name, category, ingredients, image} = cocktailOnClick
+  res.render("users/details-public",{name, category, ingredients, image})
+})
 
 module.exports = router;
