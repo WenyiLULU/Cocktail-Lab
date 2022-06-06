c7179cfff1b0d16eb219520d3c82a8eff1d3cea4
WenyiLULU <luwenyi1991@gmail.com>
2022-06-06 16:36:36 +0200

    add session and cookies
    - login page
    - logout button
    - middleware protect use routes

--- cocktail-lab/routes/auth.routes.js
+++ cocktail-lab/routes/auth.routes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
+const mongoose  = require('mongoose');
 
 const bcryptjs = require("bcryptjs");
 const saltRounds = 10;
 
 const User= require("../models/User.model.js");
-const { default: mongoose } = require('mongoose');
+
 
 // require auth middleware
 const { isLoggedIn, isLoggedOut } = require('../middleware/route.guard.js');
@@ -45,8 +46,8 @@ router.get("/login", (req, res) => {
 });
 // POST login route ==> to process form data
 router.post('/login', (req, res, next) => {
-  console.log('SESSION =====> ', req.session);
   const { email, password } = req.body;
+  console.log(req.body)
   if (email === '' || password === '') {
     res.render('auth/login', {
       errorMessage: 'Please enter both, email and password to login.'
@@ -58,16 +59,18 @@ router.post('/login', (req, res, next) => {
       if (!user) {
           // <== if there's no user with provided email
         res.render('auth/login', { errorMessage: 'Email is not registered. Try with other email.' });
+        console.log("user is :",user)
         return;
       }
     // compare this password
     // with the hashed password in database
        else if (bcryptjs.compareSync(password, user.passwordHash)) {
+         
       // if the two passwords match, render profile.ejs and
-      //  res.render('users/profile', { user });
                 //*** SAVE THE USER IN THE SESSION ****
+      console.log('SESSION =====> ', req.session);
       req.session.currentUser = user;
-      res.redirect('/profile');
+      res.redirect('/user/profile');
       } else {
       // if  passwords DON'T match
         res.render('auth/login', { errorMessage: 'Incorrect password.' });
@@ -76,6 +79,13 @@ router.post('/login', (req, res, next) => {
     .catch(error => next(error));
 }); 
 
+//Logout:
+router.post('/logout', (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) next(err);
+    res.redirect('/');
+  });
+});
 
 
 module.exports = router;
